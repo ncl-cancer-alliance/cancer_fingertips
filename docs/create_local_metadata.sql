b1fc66aa91c2d5143b047c16929d29b780d5e2e5
Jake Kealey <jake.kealey@nhs.net>
2025-09-08 15:22:14 +0100

    Bronze layer table definitions

---
+++ docs/create_local_metadata.sql
@@ -0,0 +1,9 @@
+-- Create table to store information on what fingertips data has been loaded into snowflake
+-- Contact: jake.kealey@nhs.net
+
+CREATE OR REPLACE TABLE DATA_LAKE__NCL.CANCER__FINGERTIPS.INDICATOR_UPDATE_LOG (
+    INDICATOR_ID NUMBER,
+    DATE_UPDATED_LOCAL DATE,
+    LATEST BOOLEAN,    
+	_TIMESTAMP TIMESTAMP_NTZ(9) DEFAULT CURRENT_TIMESTAMP()
+);
