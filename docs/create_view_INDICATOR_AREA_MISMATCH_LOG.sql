48eea860dbd88e24238fe0ddcf89fcf558fb238d
Jake Kealey <jake.kealey@nhs.net>
2025-10-13 19:28:17 +0100

    Updated scripts for table definitions

---
+++ docs/create_view_INDICATOR_AREA_MISMATCH_LOG.sql
@@ -0,0 +1,17 @@
+create or replace view DATA_LAKE__NCL.FINGERTIPS.INDICATOR_AREA_MISMATCH_LOG(
+	INDICATOR_ID,
+	AREA_ID,
+	AREA_TYPE_DATA,
+	AREA_TYPE_META
+) COMMENT='Flags indicators where the area boundaries are not up to date (typically PCN groups).\nContact: jake.kealey@nhs.net'
+ as
+
+SELECT DISTINCT 
+    fin."Indicator ID" AS INDICATOR_ID, fin.AREA_ID, 
+    fin."Area Type" AS AREA_TYPE_DATA, ma."Short" AS AREA_TYPE_META
+FROM DATA_LAKE__NCL.FINGERTIPS.INDICATOR_DATA fin
+
+LEFT JOIN DATA_LAKE__NCL.FINGERTIPS.METADATA_AREA ma
+ON fin.AREA_ID = ma.AREA_ID
+
+WHERE fin."Area Type" != ma."Short";
